edb40b89ff5e6020f117b030e64647db95905176
Ursachianu Ionut-Alexandru <tzmic1@gmail.com>
2023-02-07 18:43:07 +0200

    sal

--- netlify/functions/fetch-images.js
+++ netlify/functions/fetch-images.js
@@ -1,38 +1,39 @@
+const axios = require("axios");
+
 exports.handler = async (event) => {
   const { username } = JSON.parse(event.body);
 
-  const response = await fetch(
-    `https://www.reddit.com/user/${username}/submitted/.json?limit=${100}`,
-    {
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Mobile Safari/537.36",
-      },
-    }
-  );
+  try {
+    const response = await axios.get(
+      `https://www.reddit.com/user/${username}/submitted/.json?limit=${100}`,
+      {
+        headers: {
+          "User-Agent": "MyBot/0.0.1",
+        },
+      }
+    );
 
-  if (!response.ok) {
+    const data = response.data;
+    const posts = data.data.children;
+    images = posts
+      .map((post) => {
+        if (
+          post.data.url.endsWith(".jpg") ||
+          post.data.url.endsWith(".png") ||
+          post.data.url.endsWith(".gifv")
+        )
+          return post.data.url;
+      })
+      .filter((image) => image !== undefined);
+
     return {
-      statusCode: response.status,
-      body: `Failed to fetch data. Error: ${response.statusText}`,
+      statusCode: 200,
+      body: JSON.stringify({ images }),
+    };
+  } catch (error) {
+    return {
+      statusCode: error.response.status,
+      body: `Failed to fetch data. Error: ${error.response.statusText}`,
     };
   }
-
-  const data = await response.json();
-  const posts = data.data.children;
-  const images = posts
-    .map((post) => {
-      if (
-        post.data.url.endsWith(".jpg") ||
-        post.data.url.endsWith(".png") ||
-        post.data.url.endsWith(".gifv")
-      )
-        return post.data.url;
-    })
-    .filter((image) => image !== undefined);
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ images }),
-  };
 };
